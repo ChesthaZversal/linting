5cb2f443ab0062c289068b91a10e3e7ad07b6cfa
ChesthaZversal <158017676+ChesthaZversal@users.noreply.github.com>
2024-02-06 15:44:37 +0530

    Update pylinttest.py

--- pylinttest.py
+++ pylinttest.py
@@ -13,6 +13,10 @@ class TestLinting(unittest.TestCase):
         # Check if Pylint output contains specific messages
         self.assertNotIn('C0103', pylint_output, "Variable names should follow snake_case naming convention")
         self.assertNotIn('C0301', pylint_output, "Line too long")
+        pylint_score = float(pylint_output.split('Your code has been rated at ')[1].split('/')[0].strip())
+        
+        # Check if Pylint score is more than 75
+        self.assertGreaterEqual(pylint_score, 75, "Pylint score is less than 75")
 
 if __name__ == '__main__':
     unittest.main()
